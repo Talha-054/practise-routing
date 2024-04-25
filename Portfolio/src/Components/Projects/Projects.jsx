2128fac3d31ca1520ec21c25869950dfc2d80337
Talha <03345238002h@gmail.com>
2024-04-25 21:21:34 +0500

    minor code changes

--- Portfolio/src/Components/Projects/Projects.jsx
+++ Portfolio/src/Components/Projects/Projects.jsx
@@ -7,28 +7,32 @@ function Projects() {
             <div className="project-area">
 
                 <div className="p-container">
+
+                    <div className="light2"></div>
                     <div className="p1"></div>
                     <a href='https://doodle-jump-talha.vercel.app/' target='_blank'>
                         <button className='play'> Play</button>
                     </a>
-                    <div className="light2"></div>
+                    
 
                 </div>
 
                 <div className="p-container">
+                    <div className="light"></div>
                     <div className="p2"></div>
                     <a href='https://flappy-bird-talha.vercel.app' target='_blank'>
                         <button className='play'> Play</button>
                     </a>
-                    <div className="light"></div>
+                    
                 </div>
 
                 <div className="p-container">
+                    <div className="light2"></div>
                     <div className="p3"></div>
                     <a href='https://currency-converter-talha.vercel.app/' target='_blank'>
                         <button className='play'> Play</button>
                     </a>
-                    <div className="light2"></div>
+                    
                     
                 </div>
             </div>
